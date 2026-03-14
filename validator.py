4abf2fd943a95c99ce9ddc416093306efbdd1f50
Dr. Abhisakh Sarma <61180410+abhisakh@users.noreply.github.com>
2026-03-14 22:45:44 +0100

    Create validator.py

---
+++ validator.py
@@ -0,0 +1,12 @@
+import os
+from PyPDF2 import PdfReader
+
+def file_exists(file_path):
+    return os.path.exists(file_path)
+
+def file_not_empty(file_path):
+    return os.path.getsize(file_path) > 0
+
+def get_page_count(file_path):
+    reader = PdfReader(file_path)
+    return len(reader.pages)
